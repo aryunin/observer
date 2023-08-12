ee966beb2ba35f1080167f199be62346a585f7f0
aryunin <im.aryunin@gmail.com>
2023-08-12 13:32:30 +0300

    ObservablePersonCreator

--- src/main/java/person/PersonCreator.java
+++ src/main/java/person/PersonCreator.java
@@ -1,11 +1,5 @@
 package person;
 
-public class PersonCreator {
-    public Person create(PersonData data) {
-        System.out.println("Creating person " + data.getName());
-        var p = new Person();
-        p.setName(data.getName());
-        p.setEmail(data.getEmail());
-        return p;
-    }
+public interface PersonCreator {
+    Person create(PersonData data);
 }
